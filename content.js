dd1a30c53139c929e8b8afe32c5dc49c33bb47d1
Fabian Sörensson <fabian.sorensson@ictech.se>
2023-05-29 20:31:30 +0200

    Add auto finish button

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 // content.js
 
+let typedInstance = null; // Initialize a variable to hold the Typed.js instance
+
 $(document).ready(function () {
   // Hide all conversation blocks except the first one
   $('.group:not(:first)').hide();
@@ -10,7 +12,12 @@ $(document).ready(function () {
   // Append "Show next message" button to the page
   $('body').append('<button id="show-next-button" class="button">Show next message</button>');
 
+  // Append "Auto-finish message" button to the page, initially hidden
+  $('body').append('<button id="auto-finish-button" class="button" style="display: none;">Auto-finish message</button>');
+
   $('#show-next-button').click(showNextMessage);
+
+  $('#auto-finish-button').click(autoFinishMessage);
 });
 
 function showNextMessage() {
@@ -37,13 +44,43 @@ function showNextMessage() {
       showCursor: false
     };
 
+    // Save the original HTML of the message
+    nextMessage.find('.break-words').data('original-html', messageHTML);
+
     // Clear out the text of the next message and animate it
     nextMessage.find('.break-words').html('');
-    new Typed(typedTarget, options);
+    typedInstance = new Typed(typedTarget, options);  // Save the Typed.js instance
+
+    // Show the auto-finish button
+    $('#auto-finish-button').show();
   }
 
   if ($('.group:not(.displayed)').first().length == 0) {
     // When all messages have been displayed, you can choose to hide the button
     $('#show-next-button').hide();
+  }
+}
+
+function autoFinishMessage() {
+  // If there's a Typed.js instance running
+  if (typedInstance) {
+    typedInstance.destroy();  // Destroy the Typed.js instance
+
+    // Manually complete the message
+    let currentMessage = $('.group.displayed:last');
+    var originalMessageHTML = currentMessage.find('.break-words').data('original-html');
+    currentMessage.find('.break-words').html(originalMessageHTML);
+
+    // Remove the typing cursor
+    $('.typed-cursor').remove();
+
+    // Find the chat container
+    var chatContainer = document.querySelector('div.h-full.overflow-auto');
+
+    // Scroll to the bottom of the chat container
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // Hide the auto-finish button
+    $('#auto-finish-button').hide();
   }
 }
